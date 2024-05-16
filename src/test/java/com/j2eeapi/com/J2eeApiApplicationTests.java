23dad3864483962faad9e5488e9d1f5d2aceb3e5
Ludovic MARIE <ludovic.marie@aol.com>
2024-05-16 08:31:58 +0200

    init

---
+++ src/test/java/com/j2eeapi/com/J2eeApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.j2eeapi.com;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class J2eeApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
